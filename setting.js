8f217a095cd4c903d9b9e856679b970ac7d05c13
Kenz-Developer <kenzorkut49@gmail.com>
2025-01-21 07:19:28 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,7 @@
+// setting.js
+const botConfig = {
+    botToken: "7772739131:AAG0soiBKNXs5L2cuE0VG3n8H_fYCBXiWmU",  // Ganti dengan token bot Anda
+    chatId: "7299507029"  // Ganti dengan chat ID Anda
+};
+
+export default botConfig;
